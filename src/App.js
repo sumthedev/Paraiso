d0706f90d19edd16d36c2ed2c62fb3ede3bd3a11
Sumbal Hashmi <115552450+sumthedev@users.noreply.github.com>
2024-02-09 21:14:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function MobileView() {
   return (
     <div className="under-construction-container">
       <div className="under-construction-content">
-        <MdOutlineMobileOff className="construction-icon ml-[25px]" />
+        <MdOutlineMobileOff className="construction-icon ml-[30px]" />
         <h1 className="text-02">Please open on Desktop</h1>
       </div>
     </div>
